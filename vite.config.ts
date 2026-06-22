27f6f313a2f817fca6bff61c6c36dd416bcb7dce
Thomas <thomasweaverson@gmail.com>
2026-06-22 07:57:06 +0300

    feature - adds service worker

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,39 @@
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate",
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+      },
+      manifest: {
+        name: "ClockWisy",
+        short_name: "ClockWisy",
+        description: "Here you can study analog clocks and learn how to tell time using them.",
+        theme_color: "#020618",
+        background_color: "#e2e8f0",
+        display: "standalone",
+        orientation: "portrait",
+        icons: [
+          {
+            src: "/icon-192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/icon-512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+
+    })
   ],
 });
